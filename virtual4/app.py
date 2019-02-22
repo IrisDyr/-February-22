5094e188e6df8aa679204826860a26369e33942e
Iris <Irisdyrmishi@gmail.com>
2019-02-22 17:19:17 +0200

    testing

--- virtual4/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from.api.Blueprint_one import item
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-app.register_blueprint(user,url_prefix="/api/v2/item")
